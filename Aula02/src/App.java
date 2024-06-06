9a8166ce2020cc842c56ac8fb3e05fc6b6dc16e8
Damarcones Porto <damarcones@gmail.com>
2024-06-06 17:30:48 -0300

    Aula tipos variaveis e condicao

---
+++ Aula02/src/App.java
@@ -0,0 +1,72 @@
+import javax.swing.JOptionPane;
+
+public class App {
+    public static void main(String[] args) {
+        // System.out.println("Hello, World!");
+        // int inteiro = 1;
+        // double decimal = 1;
+        // float f = 1.0f;
+        // char unico = 'a
+        // String texto ="texto longo";
+        // boolean vOUf = false;
+        // byte meuByte = 00110;
+        // long numeroGrande = 999999999;
+
+
+        // int primeiro = 4;
+        // int segundo = 54;
+        // double terceiro = 45;
+        // double resultado;
+
+        // resultado = primeiro / (segundo + terceiro);
+        // System.out.println(resultado);
+        // JOptionPane.showInputDialog("Teste");
+
+
+
+
+
+
+
+        
+        // Verificação de resultado, multipliquem dois numeros e o resultado será comparado entre positivo e negativo
+        
+
+
+
+
+
+
+        String nome = "Damarcones";
+        String sobreNome = "Pôrto";
+        
+        // System.out.printf("%s Esse eh meu nome \n",nome);
+        // System.out.println(nome + "\n\n\n\n\n\n" + sobreNome);
+
+        int idade = 1000;
+
+        // if (idade == 18) {
+        //     System.out.println("Você tem 18 anos!");
+        // }
+
+        // else if (idade < 18) {
+        //     System.out.println("Você é menor de idade!");
+        //     }
+        // else
+        //     System.out.println("Você tem mais que 18 anos!");
+
+
+        int primeiro = 4;
+        int segundo = 54;
+        int resultado;
+
+        resultado = primeiro * segundo;
+
+        if(resultado > 0)
+            System.out.printf("%s, o número %d positivo.\n\n\n\n\n\n", nome, resultado);
+        else 
+            System.out.printf("Resultado negativo!");
+          
+        // JOptionPane.showMessageDialog(null, resultado);
+    }
+}
